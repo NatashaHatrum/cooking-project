7dcccc0fa3adbfef01a48e3888d87f0069f84c99
Paweł Tarka <pawel.tarka@britenet.com.pl>
2023-01-17 10:21:31 +0100

    feat/CB-2: added firebase configuration

---
+++ src/firebaseConfig.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAJ32GsgEjFRT5ykM1NyV4owuSVGMdZTG4",
+  authDomain: "cookbook-e48de.firebaseapp.com",
+  projectId: "cookbook-e48de",
+  databaseURL: "https://cookbook-e48de-default-rtdb.europe-west1.firebasedatabase.app/",
+  storageBucket: "cookbook-e48de.appspot.com",
+  messagingSenderId: "216197971973",
+  appId: "1:216197971973:web:2476e39b362f3296165c6c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
